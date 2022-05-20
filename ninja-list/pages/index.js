7c2f3fdfe08deddc13478585d6f36ab81b4ba61a
Herve Desrosiers <hdesrosiers@stingray.com>
2022-05-20 14:39:47 -0400

    06 adding styles

--- ninja-list/pages/index.js
+++ ninja-list/pages/index.js
@@ -1,25 +1,24 @@
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 import Link from 'next/link';
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod,
         molestiae perferendis porro nemo mollitia quasi accusamus voluptatum
         consequuntur tempora, veritatis sit asperiores velit quas explicabo ea?
         Tempore, optio quae?
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod,
         molestiae perferendis porro nemo mollitia quasi accusamus voluptatum
         consequuntur tempora, veritatis sit asperiores velit quas explicabo ea?
         Tempore, optio quae?
       </p>
       <Link href='/ninjas'>
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   );
