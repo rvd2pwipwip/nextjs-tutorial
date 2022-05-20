45508654d5a9c7555f24e57eb5a906bb3a54680b
Herve Desrosiers <hdesrosiers@stingray.com>
2022-05-20 07:35:33 -0400

    02 pages & routes

---
+++ ninja-list/pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return ( 
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste doloremque facere eaque. Quas dolor repellendus quidem iste voluptates eum neque ullam repudiandae inventore exercitationem, voluptatibus debitis tempora quo dolores rerum.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste doloremque facere eaque. Quas dolor repellendus quidem iste voluptates eum neque ullam repudiandae inventore exercitationem, voluptatibus debitis tempora quo dolores rerum.</p>
+    </div>
+   );
+}
+ 
+export default About;
--- ninja-list/pages/index.js
+++ ninja-list/pages/index.js
@@ -6,6 +6,8 @@ export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod, molestiae perferendis porro nemo mollitia quasi accusamus voluptatum consequuntur tempora, veritatis sit asperiores velit quas explicabo ea? Tempore, optio quae?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod, molestiae perferendis porro nemo mollitia quasi accusamus voluptatum consequuntur tempora, veritatis sit asperiores velit quas explicabo ea? Tempore, optio quae?</p>
     </div>
   )
 }
