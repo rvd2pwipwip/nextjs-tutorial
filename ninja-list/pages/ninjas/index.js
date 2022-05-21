703e8d21a095648add23e0adee56aeb3ebb8ee83
Herve Desrosiers <hdesrosiers@stingray.com>
2022-05-21 14:40:08 -0400

    10 fetching data

--- ninja-list/pages/ninjas/index.js
+++ ninja-list/pages/ninjas/index.js
@@ -1,9 +1,28 @@
-const Ninjas = () => {
-  return ( 
+import styles from '../../styles/Ninjas.module.css';
+import Link from 'next/link';
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
+  return (
     <div>
-      <h1>Ninjas</h1>
+      <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </Link>
+      ))}
     </div>
-   );
-}
- 
+  );
+};
+
 export default Ninjas;
