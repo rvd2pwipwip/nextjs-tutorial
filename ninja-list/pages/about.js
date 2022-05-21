dcf9aa4f2bad5f941900243aaf85a04bcabe132d
Herve Desrosiers <hdesrosiers@stingray.com>
2022-05-21 06:30:43 -0400

    08 redirecting users

--- ninja-list/pages/about.js
+++ ninja-list/pages/about.js
@@ -1,11 +1,29 @@
+import Head from 'next/head';
+
 const About = () => {
-  return ( 
-    <div>
-      <h1>About</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste doloremque facere eaque. Quas dolor repellendus quidem iste voluptates eum neque ullam repudiandae inventore exercitationem, voluptatibus debitis tempora quo dolores rerum.</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste doloremque facere eaque. Quas dolor repellendus quidem iste voluptates eum neque ullam repudiandae inventore exercitationem, voluptatibus debitis tempora quo dolores rerum.</p>
-    </div>
-   );
-}
- 
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='keywords' content='about' />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste
+          doloremque facere eaque. Quas dolor repellendus quidem iste voluptates
+          eum neque ullam repudiandae inventore exercitationem, voluptatibus
+          debitis tempora quo dolores rerum.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste
+          doloremque facere eaque. Quas dolor repellendus quidem iste voluptates
+          eum neque ullam repudiandae inventore exercitationem, voluptatibus
+          debitis tempora quo dolores rerum.
+        </p>
+      </div>
+    </>
+  );
+};
+
 export default About;
--- ninja-list/pages/index.js
+++ ninja-list/pages/index.js
@@ -1,25 +1,32 @@
+import Head from 'next/head';
 import Link from 'next/link';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod,
-        molestiae perferendis porro nemo mollitia quasi accusamus voluptatum
-        consequuntur tempora, veritatis sit asperiores velit quas explicabo ea?
-        Tempore, optio quae?
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod,
-        molestiae perferendis porro nemo mollitia quasi accusamus voluptatum
-        consequuntur tempora, veritatis sit asperiores velit quas explicabo ea?
-        Tempore, optio quae?
-      </p>
-      <Link href='/ninjas'>
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod,
+          molestiae perferendis porro nemo mollitia quasi accusamus voluptatum
+          consequuntur tempora, veritatis sit asperiores velit quas explicabo
+          ea? Tempore, optio quae?
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quod,
+          molestiae perferendis porro nemo mollitia quasi accusamus voluptatum
+          consequuntur tempora, veritatis sit asperiores velit quas explicabo
+          ea? Tempore, optio quae?
+        </p>
+        <Link href='/ninjas'>
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
